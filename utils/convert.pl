f316802f3b72a5ccd3100efe4096bc3ca3acd0ef
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2024-09-03 15:00:04 +0800

    Create convert.pl

---
+++ utils/convert.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+
+my $nametxt = shift;
+my $gff = shift;
+open I, "<$nametxt" or die "Usage: perl $0 nametxt gff3";
+
+my %data;
+while (<I>) 
+{
+        chomp;
+        my @row = split(/\t/);
+        $data{$row[0]} = $row[1];
+}
+
+open I1, "<$gff" or die "";
+while (<I1>)
+{
+        #chomp;
+        foreach my $key (keys %data)
+        {
+                $_ =~ s/$key/$data{$key}/g;
+        }
+        print;
+}
