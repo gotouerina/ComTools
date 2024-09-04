124297baff29223fd2a411a9da4332e26e8e0229
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2024-09-04 14:26:14 +0800

    Update lst2fasta.pl

--- utils/lst2fasta.pl
+++ utils/lst2fasta.pl
@@ -1,15 +1,20 @@
+#################lst2fasta.pl###############
+##--------------------------------------####
+########make by Kogoori Masaki##############
+##--------------------------------------####
+####Usage: perl $0 speciesnumber lst fasta##
 #! /usr/bin/perl
 use strict;
 use warnings;
-my @species = ("Scaptochirus","Condylura","Dymecodon","Euroscaptor","Galemys","Neurotrichus","Parascalops","Parascaptor","Scalopus","Scapanulus","Scapanus","Scaptonyx","Sorex","Teuropaea","Toccidentalis","Uropsilus","Mogera","Urotrichus");
+my $species = shift;
 my $lst = shift;
-#open I, "<$lst" or die "Usage: perl $0 lst fasta";
+#open I, "<$lst" or die "Usage: perl $0 speciesnumber lst fasta";
 
 my $i = 2;
-for ($i=2; $i<=(@species+1); $i++)
+for ($i=2; $i<=($species+1); $i++)
 {
         my $d = 1;
-        open I, "<$lst" or die "Usage: perl $0 lst fasta";
+        open I, "<$lst" or die "Usage: Usage: perl $0 speciesnumber lst fasta";
         print ">";
         while (<I>)
         {
