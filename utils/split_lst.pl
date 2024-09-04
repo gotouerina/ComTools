37632babce623ce7464c7c72f858ea694a1f0848
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2024-09-04 14:27:17 +0800

    Update split_lst.pl

--- utils/split_lst.pl
+++ utils/split_lst.pl
@@ -1,8 +1,13 @@
+#################split_lst.pl###############
+##--------------------------------------####
+########make by Kogoori Masaki##############
+##--------------------------------------####
+######Usage: perl $0 lst_file chr_name######
 #! /usr/bin/perl
 use strict;
 use warnings;
 my $lst = shift;
-open I,"<$lst" or die "Usage: perl $0 lst select_chr";
+open I,"<$lst" or die "Usage: perl $0 lst_file chr_name";
 
 my $chr = shift;
 open O,">$chr.lst" or die "";
